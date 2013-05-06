6814936d174fdc4d9cc7d052b03a4b1a3ec78d63
Andrei Helo Güereca <andreihelo@gmail.com>
2013-05-05 23:10:30 -0600

    Finish sign in.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -123,5 +124,10 @@ describe User do
       # specify is a synonym of it and is used when writing it sounds unnatural.
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
